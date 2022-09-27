6400039c7d8d9e23b566de3fc5b48fadefee64ea
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-27 17:22:56 -0300

    :sparkles: feat: create controller for authentication

---
+++ src/controllers/authController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+
+async function signup(req: Request, res: Response) {
+  // TODO implement signup
+}
+
+const authController = {
+  signup,
+};
+
+export default authController;
--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import validateSchema from '../middlewares/validateSchema';
+import authController from '../controllers/authController';
 import signupSchema from '../schemas/authSchema';
 
 const authRouter = Router();
 
-authRouter.post('/signup', validateSchema(signupSchema));
+authRouter.post('/signup', validateSchema(signupSchema), authController.signup);
 
 export default authRouter;
