ca4835d27218fc3f052f14d5acc67d6381cd588f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-27 14:36:35 -0300

    :sparkles: feat: archive app and archive index were created

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import chalk from 'chalk';
+import app from './app'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const port = +process.env.PORT || 9000
+
+app.listen(port, () => {
+  console.log('')
+  console.log(chalk.green.bold(`Server is up and running on port ${port}`))
+  console.log(
+    chalk.yellow.bold(`Mode: ${process.env.MODE || 'not defined -> DEV'}`),
+  )
+  console.log(chalk.yellow(`Verbose: ${process.env.VERBOSE || 'false'}`))
+  console.log('---------------------------------------')
+})
