feb0a1c2160c493749b8805ec45dcbaf31f66da3
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-27 15:45:18 -0300

    :wrench: chore: modified settings for prettier

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
-import express from 'express'
-import cors from 'cors'
-import 'express-async-errors'
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 
 // import middlewares
 
 // import routes
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-export default app
+export default app;
