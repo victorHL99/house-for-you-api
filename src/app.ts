bb475f57373efcc713ef1ffb1a27db6c3fc43e06
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-27 17:04:38 -0300

    :sparkles: feat: create error handler and create index for routes

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import cors from 'cors';
 import 'express-async-errors';
 
 // import middlewares
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 
 // import routes
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
+app.use(errorHandlerMiddleware);
 
 export default app;
