11b2678e2451e9056c06088b7c4f3e644e35c292
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-30 14:20:34 -0300

    :sparkles: feat: create announcements router

--- src/routes/announcementsRoutes.ts
+++ src/routes/announcementsRoutes.ts
@@ -1,5 +1,14 @@
 import { Router } from 'express';
+import validateSchema from '../middlewares/validateSchema';
+import validateToken from '../middlewares/validateToken';
+import announcementSchema from '../schemas/announcementSchema';
 
 const announcementsRouter = Router();
 
+announcementsRouter.post(
+  '/announcements',
+  validateToken,
+  validateSchema(announcementSchema.createAnnouncementSchema),
+  announcementController.createAnnouncement,
+);
 export default announcementsRouter;
