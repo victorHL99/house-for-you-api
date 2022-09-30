6f54175a8832e72969d8728b9c537b4508f07d6f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-29 22:29:32 -0300

    :sparkles: feat: added router for announcements

---
+++ src/routes/announcementsRoutes.ts
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const announcementsRouter = Router();
+
+export default announcementsRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import authRouter from './authRouter';
+import announcementsRouter from './announcementsRoutes';
 
 const routes = Router();
 
 routes.use(authRouter);
+routes.use('/home', announcementsRouter);
 
 export default routes;
