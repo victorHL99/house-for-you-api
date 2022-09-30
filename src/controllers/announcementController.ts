7d23d09bf746aa1408cfaf57d8df4a8250d7d5e1
victorHL99 <victor.lessa@estudante.ufla.br>
2022-09-30 14:45:34 -0300

    :sparkles: feat: create function for return userId by email

---
+++ src/controllers/announcementController.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express';
+import announcementService from '../services/announcementService';
+import { CreateAnnouncement } from '../types/announcementInterface';
+
+async function createAnnouncement(req: Request, res: Response) {
+  const {
+    value,
+    number_house,
+    description,
+    area,
+    bedrooms,
+    garage,
+    bathrooms,
+    phone_number,
+    address,
+  } = req.body as CreateAnnouncement;
+  const token = req.headers.authorization.split(' ')[1] as string;
+
+  // get userId from token
+  const email = await announcementService.decryptToken(token);
+  console.log(email);
+}
+const announcementController = {
+  createAnnouncement,
+};
+
+export default announcementController;
