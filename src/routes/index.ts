c5e3e563e9740a40f97f11c2b5075b7defffdbfd
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-07 13:50:36 -0300

    :sparkles: feat: create userRouter for return info at the user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import authRouter from './authRouter';
 import announcementsRouter from './announcementsRoutes';
+import userRouter from './userRoutes';
 
 const routes = Router();
 
 routes.use(authRouter);
 routes.use('/home', announcementsRouter);
+routes.use('/user', userRouter);
 
 export default routes;
