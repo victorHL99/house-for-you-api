231af89ec8b4635feed0bad6b5560a4fe6ae5558
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-09 18:21:27 -0300

    :sparkles: feat: create repositorie for create picture

---
+++ src/repositories/picturesRepository.ts
@@ -0,0 +1,36 @@
+import client from '../config/database';
+import { CreatePicture } from '../types/picturesInterface';
+
+async function getUserByEmail(email: string) {
+  const user = await client.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  return user.id;
+}
+
+async function getAnnouncementById(id: number) {
+  const announcement = await client.announcement.findFirst({
+    where: {
+      id,
+    },
+  });
+
+  return announcement;
+}
+
+async function createPicture(picture: CreatePicture) {
+  await client.picture.create({
+    data: picture,
+  });
+}
+
+const picturesRepository = {
+  getUserByEmail,
+  getAnnouncementById,
+  createPicture,
+};
+
+export default picturesRepository;
