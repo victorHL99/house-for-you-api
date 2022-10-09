3e27acee31ad3ef0e74436e69c219f61c3531d81
victorHL99 <victor.lessa@estudante.ufla.br>
2022-10-09 18:22:15 -0300

    :sparkles: feat: added router for picture at the principal router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,11 +3,13 @@ import { Router } from 'express';
 import authRouter from './authRouter';
 import announcementsRouter from './announcementsRoutes';
 import userRouter from './userRoutes';
+import picturesRouter from './picturesRoutes';
 
 const routes = Router();
 
 routes.use(authRouter);
 routes.use('/home', announcementsRouter);
 routes.use('/user', userRouter);
+routes.use('/pictures', picturesRouter);
 
 export default routes;
